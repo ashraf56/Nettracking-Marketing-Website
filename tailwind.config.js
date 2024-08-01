71245c1a6feae9ef628212433027b422fdc8129d
ashraf56 <ashrafulfahim07@gmail.com>
2024-08-01 16:59:19 +0600

    faq completed

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        boxShadow: {
+          'custom-shadow': '0px 50px 160px -40px #0000000A',
+        },
+      },
+    },
+  };
+  
